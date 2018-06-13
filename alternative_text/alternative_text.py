340102cde211548e7c4f2508861219af57da8f96
Martin Yanchev <martinm199913@gmail.com>
2018-06-13 12:33:01 +0300

    Problem with import String

--- alternative_text/alternative_text.py
+++ alternative_text/alternative_text.py
@@ -1,14 +1,1 @@
-def to_alternating_case(string):
-    new_string = ""
-
-    for char in string:
-        if char == char.upper():
-            char = char.lower()
-            new_string += '-'.join(char)
-        else:
-            char = char.upper()
-            new_string += ''.join(char)
-
-    return new_string
-    #your code here
-print(to_alternating_case("Hello World"))
+from  string import swapcase as to_alternating_case

2a7d6f4a4aa20d1e67a60f47d3a2b671715802f4
Martin Yanchev <martinm199913@gmail.com>
2018-06-13 12:35:06 +0300

    Back to normal version

--- alternative_text/alternative_text.py
+++ alternative_text/alternative_text.py
@@ -1,1 +1,14 @@
-from  string import swapcase as to_alternating_case
+def to_alternating_case(string):
+    new_string = ""
+
+    for char in string:
+        if char == char.upper():
+            char = char.lower()
+            new_string += '-'.join(char)
+        else:
+            char = char.upper()
+            new_string += ''.join(char)
+
+    return new_string
+    #your code here
+print(to_alternating_case("Hello World"))
